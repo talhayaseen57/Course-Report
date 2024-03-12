88c346fc0838f71d8da540a2a68839d12bddcdad
talhayaseen57 <talhayaseen5725@gmail.com>
2024-03-12 15:00:50 +0100

    Created spring app

---
+++ src/main/java/com/sherenterprise/coursereport/CourseReportApplication.java
@@ -0,0 +1,13 @@
+package com.sherenterprise.coursereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sherenterprise/coursereport/CourseReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherenterprise.coursereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
