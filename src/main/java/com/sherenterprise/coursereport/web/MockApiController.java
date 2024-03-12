ae1ee08bb4e99824451e4d63e22ac0da87b1b9e3
talhayaseen57 <talhayaseen5725@gmail.com>
2024-03-12 15:45:55 +0100

    created get-mock-api endpoint

---
+++ src/main/java/com/sherenterprise/coursereport/web/MockApiController.java
@@ -0,0 +1,15 @@
+package com.sherenterprise.coursereport.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MockApiController {
+
+    @GetMapping("/get-mock-api")
+    public ResponseEntity<?> fetchData() {
+
+        return ResponseEntity.ok("OKAY");
+    }
+}
