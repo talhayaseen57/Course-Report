2432ee2d42581b6353cf113ebe88b641fc2dc191
talhayaseen57 <talhayaseen5725@gmail.com>
2024-03-13 23:59:22 +0100

    created a scheduler
    A scheduler is created which triggers CourseReportService for each 5 minutes

---
+++ src/main/java/com/sherenterprise/coursereport/cronjob/CourseReportScheduler.java
@@ -0,0 +1,23 @@
+package com.sherenterprise.coursereport.cronjob;
+
+import com.sherenterprise.coursereport.service.CourseReportService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CourseReportScheduler {
+
+    private CourseReportService courseReportService;
+
+    public CourseReportScheduler(CourseReportService courseReportService) {
+        this.courseReportService = courseReportService;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void pollCourseReportApi() {
+        courseReportService.pollCourseReportApi();
+    }
+
+}
