ba355809a947550a63f3d1fa4a51b84c41da731c
talhayaseen57 <talhayaseen5725@gmail.com>
2024-03-20 02:33:44 +0100

    added configuration file

---
+++ src/main/java/com/sherenterprise/coursereport/service/SlackSecrets.java
@@ -0,0 +1,6 @@
+package com.sherenterprise.coursereport.service;
+
+public class SlackSecrets {
+    public static final String SLACK_TOKEN = "";    // enter your slack app token
+    public static final String CHANNEL_ID = "";     // enter the slack channel id
+}
